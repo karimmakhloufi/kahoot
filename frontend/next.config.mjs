c5732ced67677dc7dcdd8ed37a95677451b23744
Karim Makhloufi <karim.makhloufi.paris@gmail.com>
2024-09-06 18:11:45 +0200

    Removed redirects

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -1,26 +1,15 @@
-import createMDX from '@next/mdx'
+import createMDX from "@next/mdx";
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  pageExtensions: ['js', 'jsx', 'md', 'mdx', 'ts', 'tsx'],
+  pageExtensions: ["js", "jsx", "md", "mdx", "ts", "tsx"],
   async redirects() {
-    return [
-      {
-        source: '/',
-        destination: '/host/dashboard',
-        permanent: true,
-      },
-      {
-        source: '/host',
-        destination: '/host/dashboard',
-        permanent: true,
-      },
-    ]
+    return [];
   },
-}
+};
 
 const withMDX = createMDX({
   // Add markdown plugins here, as desired
-})
+});
 
-export default withMDX(nextConfig)
+export default withMDX(nextConfig);
